7110ea099750ac650b5d25575d0d85512ba0acf9
Nnihilnovumsubsole <nihilnovumsubsole@outlook.es>
2026-08-01 12:26:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const pages = document.getElementById("pages");
+
+pages.addEventListener("click", () => {
+    window.open(
+        "https://docs.google.com/document/d/1bK4KNGjiNNFwAMSJdq4UEPStXAU1IAWC8RcK1y0dx8s/edit?pli=1&tab=t.sg9tgtn4n9jp#heading=h.o9qf44iunrvf",
+        "_blank"
+    );
+});
